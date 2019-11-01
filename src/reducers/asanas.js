4006c5d5d96951bbcc268da83721653509331e10
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2019-11-01 22:11:25 +0400

    Вынес массив асан в отдельный файл

---
+++ src/reducers/asanas.js
@@ -0,0 +1,38 @@
+export const asanas = [
+  {
+    asanaName: {
+      ru: "Тадасана"
+    },
+    asanaImg: "/img/tadasana.jpg"
+  },
+  {
+    asanaName: {
+      ru: "Уттхита Триконасана"
+    },
+    asanaImg: "/img/utrikonasana.jpg"
+  },
+  {
+    asanaName: {
+      ru: "Вирабхадрасана 3"
+    },
+    asanaImg: "/img/vira3.jpg"
+  },
+  {
+    asanaName: {
+      ru: "Вирабхадрасана 2"
+    },
+    asanaImg: "/img/vira2.jpg"
+  },
+  {
+    asanaName: {
+      ru: "Прасарита Падоттанасана"
+    },
+    asanaImg: "/img/prasarita.jpg"
+  },
+  {
+    asanaName: {
+      ru: "Врикшасана"
+    },
+    asanaImg: "/img/vrikshasana.jpg"
+  }
+];
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { asanas } from "./asanas";
+
 export const initialState = {
   language: "ru",
   schedule: {
@@ -14,44 +16,7 @@ export const initialState = {
     fastTransition: false,
     onPlaceHolder: false
   },
-  asanas: [
-    {
-      asanaName: {
-        ru: "Тадасана"
-      },
-      asanaImg: "/img/tadasana.jpg"
-    },
-    {
-      asanaName: {
-        ru: "Уттхита Триконасана"
-      },
-      asanaImg: "/img/utrikonasana.jpg"
-    },
-    {
-      asanaName: {
-        ru: "Вирабхадрасана 3"
-      },
-      asanaImg: "/img/vira3.jpg"
-    },
-    {
-      asanaName: {
-        ru: "Вирабхадрасана 2"
-      },
-      asanaImg: "/img/vira2.jpg"
-    },
-    {
-      asanaName: {
-        ru: "Прасарита Падоттанасана"
-      },
-      asanaImg: "/img/prasarita.jpg"
-    },
-    {
-      asanaName: {
-        ru: "Врикшасана"
-      },
-      asanaImg: "/img/vrikshasana.jpg"
-    }
-  ]
+  asanas: asanas
 };
 
 export function rootReducer(state = initialState, action) {
