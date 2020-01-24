a85a35a0095b4da6133302c2f116de9bbdb7b0c8
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-01-24 20:56:02 +0400

    Переписал стили в App

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,27 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
 import AsanasPanel from "../containers/AsanasPanel";
 import SchedulePanel from "../containers/SchedulePanel";
 import DnDContext from "../containers/DnDContext";
 import PageTop from "../containers/PageTop";
 import "./App.css";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: "100%"
-  },
-  navPanel: {
-    height: "10%"
-  },
-  gridPanel: {
-    height: "90%"
-  }
-}));
-
 export default function App(props) {
-  const classes = useStyles();
-
   return (
     <React.Fragment>
       <div className="App">
         <CssBaseline />
         <DnDContext>
-          <Container maxWidth="lg" className={classes.root}>
-            <Grid container spacing={3} className={classes.root}>
-              <Grid item xs={12} className={classes.navPanel}>
+          <Container maxWidth="lg" style={{ height: "100%" }}>
+            <Grid container spacing={3} style={{ height: "100%" }}>
+              <Grid item xs={12} style={{ height: "10%" }}>
                 <PageTop />
               </Grid>
-              <Grid item xs={6} className={classes.gridPanel}>
+              <Grid item xs={6} style={{ height: "90%" }}>
                 <AsanasPanel />
               </Grid>
-              <Grid item xs={6} className={classes.gridPanel}>
+              <Grid item xs={6} style={{ height: "90%" }}>
                 <SchedulePanel />
               </Grid>
             </Grid>
