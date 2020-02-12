faa3423f83e581956669204befd5055a7dd6388b
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-02-12 12:51:42 +0400

    Создал компонент LanguageChooser

---
+++ src/components/LanguageChooser.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./LanguageChooser.css";
+
+export default function LanguageChooser(props) {
+  return <div>Рус</div>;
+}
--- src/containers/PageTop.js
+++ src/containers/PageTop.js
@@ -3,10 +3,15 @@ import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import LanguageChooser from "../components/LanguageChooser";
 import "./PageTop.css";
 
 const useStyles = makeStyles(theme => ({
-  root: {}
+  root: {},
+  instruction: {
+    margin: theme.spacing(0, 2)
+  }
 }));
 
 function PageTop(props) {
@@ -14,10 +19,12 @@ function PageTop(props) {
 
   return (
     <Paper className={classes.root}>
+      <LanguageChooser language="ru" />
+      <Divider variant="middle" />
       <Typography variant="h4" component="h1" align="center">
         {props.pageTop.headerText[props.language]}
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" className={classes.instruction}>
         {props.pageTop.instructionText[props.language]}
       </Typography>
     </Paper>
