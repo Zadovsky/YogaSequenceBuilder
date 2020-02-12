6cdb0ac820b92e085aa4a624de8e97695094db70
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-02-12 17:44:10 +0400

    Оформил LanguageChooser

--- src/components/LanguageChooser.js
+++ src/components/LanguageChooser.js
@@ -1,6 +1,23 @@
 import React from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import "./LanguageChooser.css";
 
 export default function LanguageChooser(props) {
-  return <div>Рус</div>;
+  return (
+    <Select value="ru">
+      <MenuItem value="ru">
+        <div className="langItemWrapper">
+          <img src="/img/rusflag.png" alt="flag"></img>
+          <p>Рус</p>
+        </div>
+      </MenuItem>
+      <MenuItem value="en">
+        <div className="langItemWrapper">
+          <img src="/img/engflag.png" alt="flag"></img>
+          <p>Eng</p>
+        </div>
+      </MenuItem>
+    </Select>
+  );
 }
