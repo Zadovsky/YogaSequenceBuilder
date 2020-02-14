3afb76183a822dd8ddd705da5353e210491ef7e7
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-02-14 18:26:09 +0400

    Улучшил верстку App

--- src/components/App.js
+++ src/components/App.js
@@ -6,26 +6,41 @@ import AsanasPanel from "../containers/AsanasPanel";
 import SchedulePanel from "../containers/SchedulePanel";
 import DnDContext from "../containers/DnDContext";
 import PageTop from "../containers/PageTop";
+import { makeStyles } from "@material-ui/core/styles";
 import "./App.css";
 
+const useStyles = makeStyles(theme => ({
+  grid: {
+    height: "100%"
+  }
+}));
+
 export default function App(props) {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <div className="App">
         <CssBaseline />
         <DnDContext>
           <Container maxWidth="lg" style={{ height: "100%" }}>
-            <Grid container spacing={3} style={{ height: "100%" }}>
-              <Grid item xs={12} style={{ height: "17%" }}>
-                <PageTop />
+            <div className="TopPanelsFlexBox">
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <PageTop />
+                </Grid>
               </Grid>
-              <Grid item xs={6} style={{ height: "83%" }}>
-                <AsanasPanel />
-              </Grid>
-              <Grid item xs={6} style={{ height: "83%" }}>
-                <SchedulePanel />
-              </Grid>
-            </Grid>
+              <div className="TopPanelsFlexElement">
+                <Grid container spacing={3} className={classes.grid}>
+                  <Grid item xs={6} className={classes.grid}>
+                    <AsanasPanel />
+                  </Grid>
+                  <Grid item xs={6} className={classes.grid}>
+                    <SchedulePanel />
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
           </Container>
         </DnDContext>
       </div>
