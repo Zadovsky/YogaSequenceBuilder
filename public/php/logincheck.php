cc1d0b803a96c7509f258418500b9974568bb1b7
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-03-05 17:13:52 +0400

    Сделал запрос авторизации mysql

--- public/php/logincheck.php
+++ public/php/logincheck.php
@@ -1,8 +1,21 @@
 <?php
 $data = json_decode(file_get_contents('php://input'), true);
-if ($data['email'] == '1') {
-    $answer = true;
-} else {
-    $answer = false;
+$mysqli = new mysqli("localhost", "root", "", "ysb");
+
+if ($mysqli->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+
+$res = $mysqli->query("SELECT * FROM users WHERE login = '" . $data['email'] . "' AND password = '" . $data['password'] . "'");
+
+if (!$res) {
+    echo "Ошибка: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+if ($res->num_rows == 0) {
+    $answer = false;
+} else {
+    $answer = true;
+}
+
 echo (json_encode($answer));
