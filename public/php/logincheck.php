9cb409fae7f100bb055b275532019ea06adbdbf7
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-03-05 17:27:50 +0400

    Вынес реквизиты доступа к базе в config.php

--- public/php/logincheck.php
+++ public/php/logincheck.php
@@ -1,6 +1,8 @@
 <?php
+require_once('config.php');
+
 $data = json_decode(file_get_contents('php://input'), true);
-$mysqli = new mysqli("localhost", "root", "", "ysb");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($mysqli->connect_errno) {
     echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
