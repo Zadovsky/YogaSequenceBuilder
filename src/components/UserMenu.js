8aba2562bc280b57b26fc7a31eae61524c622e99
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-05-11 20:34:20 +0400

    Добавил иконку рядом с логином

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,11 +2,15 @@ import React from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
+import "./UserMenu.css";
 
 export default function UserMenu(props) {
   return (
     <div className="UserMenu">
-      <Button onClick={props.usernameClickAction}>{props.login}</Button>
+      <Button onClick={props.usernameClickAction}>
+        <img src="/img/user.png" alt="user" className="userIcon" />
+        {props.login}
+      </Button>
       <Menu
         anchorEl={props.anchorEl}
         open={props.isOpen}
