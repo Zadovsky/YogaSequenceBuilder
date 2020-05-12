68c1fd334a4751a2ffb54fbc0cedaa6992bdf0ec
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-05-12 19:51:54 +0400

    Добавил margin у кнопок навигации

--- src/components/AsanasNavigation.js
+++ src/components/AsanasNavigation.js
@@ -3,21 +3,25 @@ import Chip from "@material-ui/core/Chip";
 import "./AsanasNavigation.css";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   asanasNavigation: {
-    margin: theme.spacing(0, 0, 1)
-  }
+    margin: theme.spacing(0, 0, 1),
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
 }));
 
 export default function AsanasNavigation(props) {
   const classes = useStyles();
 
-  const menuItems = props.groups.map(group => {
+  const menuItems = props.groups.map((group) => {
     return (
       <Chip
         variant="outlined"
         key={group.id}
         label={group.name}
+        className={classes.chip}
         onClick={() => {
           props.onClick(group.id);
         }}
