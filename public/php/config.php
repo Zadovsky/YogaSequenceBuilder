1816fc123f90e5f3aba21a6d24dd7a9b794fa5d4
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-08-19 15:27:50 +0400

    Изменил логин и пароль к БД

--- public/php/config.php
+++ public/php/config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'srv38871_prpo');
+define('DB_PASS', 'gkwof32!');
 define('DB_NAME', 'ysb');
