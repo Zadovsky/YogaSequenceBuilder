053d5a6e27a7297f35dadb53e90dfea6a1d85284
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-09-24 20:16:53 +0400

    Сделал правильный размер GhostBlock

--- src/components/GhostBlock.js
+++ src/components/GhostBlock.js
@@ -24,6 +24,14 @@ export default class GhostBlock extends React.Component {
     return (
       <div
         className="GhostBlock"
+        style={
+          this.props.target
+            ? {
+                width: this.props.target.offsetWidth,
+                height: this.props.target.offsetHeigh,
+              }
+            : null
+        }
         dangerouslySetInnerHTML={{
           __html: this.props.target ? this.props.target.innerHTML : null,
         }}
