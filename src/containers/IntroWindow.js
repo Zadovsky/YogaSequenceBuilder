1a787894e1a74030bc0dae2c462f891245526680
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-13 20:24:47 +0400

    Добавил кнопку закрытия IntroWIndow

--- src/containers/IntroWindow.js
+++ src/containers/IntroWindow.js
@@ -6,11 +6,39 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
+import { useMediaQuery } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 import { onCloseIntroWindow } from "../actions/IntroWindowActions";
 
+const useStyles = makeStyles((theme) => ({
+  closeButton: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+  },
+}));
+
 function IntroWindow(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const closeIconButtonProps = {
+    size: isSmallScreen ? "small" : undefined,
+  };
+
   return (
     <Dialog open={props.intro.isOpen} onClose={props.onCloseIntroWindow}>
+      <IconButton
+        {...closeIconButtonProps}
+        className={classes.closeButton}
+        onClick={props.onCloseIntroWindow}
+      >
+        <CloseIcon />
+      </IconButton>
       <DialogTitle>
         {props.intro.texts[0][props.language.curLang].title}
       </DialogTitle>
