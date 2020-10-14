b24a3a8cb1d225f4709817ab6a2df67794b9f816
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-14 17:08:17 +0400

    Прописал тексты для IntroWindow

--- src/reducers/intro.js
+++ src/reducers/intro.js
@@ -10,22 +10,48 @@ const initialState = {
   texts: [
     {
       ru: {
-        title: "Заголовок 1",
-        text: "Текст 1",
+        title: "Что такое Yoga Sequence Builder?",
+        text:
+          "Это приложение для планирования последовательностей асан практики хатха-йоги",
       },
       en: {
-        title: "Title 1",
-        text: "Text 1",
+        title: "What is Yoga Sequence Builder?",
+        text: "The application will help you to plan your yoga practice",
       },
     },
     {
       ru: {
-        title: "Заголовок 2",
-        text: "Текст 2",
+        title: "Как это работает?",
+        text:
+          "Перетаскивайте карточки из списка асан в панель расписания. Меняйте асаны и блоки асан местами. На мобильном устройстве для начала перетаскивания удерживайте нажатие в течении 1сек.",
       },
       en: {
-        title: "Title 2",
-        text: "Text 2",
+        title: "How it works?",
+        text:
+          "Drag’n’drop cards from asanas list to schedule panel. Swap asanas and asanas blocks. On mobile devices to start drag’n’drop hold your touch for 1sec.",
+      },
+    },
+    {
+      ru: {
+        title: "Авторизуйтесь",
+        text:
+          "Авторизуйтесь и сохраняйте комплексы асан в личную коллекцию. Загружайте, редактируйте и сохраняйте комплексы асан когда это вам потребуется.",
+      },
+      en: {
+        title: "Log in",
+        text:
+          "Log in and save your sequences to collection. Load, edit and save it any time you need.",
+      },
+    },
+    {
+      ru: {
+        title: "PDF",
+        text:
+          "Сохраняйте комплексы асан в PDF формате для распечатки или просмотра на мобильном устройстве.",
+      },
+      en: {
+        title: "PDF",
+        text: "Save your sequences to pdf to print or view on mobile device.",
       },
     },
   ],
