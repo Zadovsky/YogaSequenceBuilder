7a26db9da143137e496d72492b0599901ad1bb1d
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-20 18:35:05 +0400

    Изменил поведение при SET_LANG_BY_IP

--- src/reducers/language.js
+++ src/reducers/language.js
@@ -14,19 +14,26 @@ const initialState = {
     },
   },
   curLang: "en",
+  langFromCookies: false,
 };
 
 export function languageReducer(state = initialState, action) {
   switch (action.type) {
     case SET_LANG_BY_IP:
-      return { ...state, curLang: action.payload };
+      if (!state.langFromCookies) {
+        return { ...state, curLang: action.payload };
+      } else return state;
 
     case CHANGE_LANG:
       return { ...state, curLang: action.payload };
 
     case GET_COOKIES:
       if (action.payload.lang) {
-        return { ...state, curLang: action.payload.lang };
+        return {
+          ...state,
+          curLang: action.payload.lang,
+          langFromCookies: true,
+        };
       } else return state;
 
     default:
