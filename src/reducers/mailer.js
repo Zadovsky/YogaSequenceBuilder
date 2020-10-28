364bf5353da53d6c3358292512c53a5d27ba0d06
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-28 16:58:53 +0400

    Добавил функцию замены ключевого слова в тексте

--- src/reducers/mailer.js
+++ src/reducers/mailer.js
@@ -7,15 +7,35 @@ const initialState = {
   texts: null,
   loginRegedText: {
     ru: {
-      subj: "Тест",
-      text: "тест",
+      subj: "Реквизиты для входа в аккаунт YogaSequenceBuilder.online",
+      text:
+        "Добрый день! <br> Ваш аккаунт YogaSequenceBuilder.online зарегистрирован на этот адрес. <br> Пароль для входа: #PASSWORD <br> Желаем хорошей практики!",
     },
     en: {
-      subj: "Test",
-      text: "test",
+      subj: "YogaSequenceBuilder.online registration information",
+      text:
+        "Hello! <br> Your YogaSequenceBuilder.online account is registered on this e-mail. <br> Password to enter is: #PASSWORD <br> Have a good practice!",
     },
   },
 };
+
+function changeKeyWord(text, keyWord, exchange) {
+  let resultText = {};
+  for (let key1 in text) {
+    resultText[key1] = {};
+    for (let key2 in text[key1]) {
+      const curStr = text[key1][key2];
+      let keyWordIndex = curStr.indexOf(keyWord);
+      if (keyWordIndex > -1) {
+        resultText[key1][key2] =
+          curStr.slice(0, keyWordIndex) +
+          exchange +
+          curStr.slice(keyWordIndex + keyWord.length);
+      } else resultText[key1][key2] = curStr;
+    }
+  }
+  return resultText;
+}
 
 export function mailerReducer(state = initialState, action) {
   switch (action.type) {
@@ -24,7 +44,11 @@ export function mailerReducer(state = initialState, action) {
         ...state,
         sent: true,
         email: action.payload.login,
-        texts: state.loginRegedText,
+        texts: changeKeyWord(
+          state.loginRegedText,
+          "#PASSWORD",
+          action.payload.password
+        ),
       };
 
     case SENT_MAIL:
