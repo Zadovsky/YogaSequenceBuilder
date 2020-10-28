602cdfae62ce3d7f81f3dea3c3557c3403132430
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-28 22:30:31 +0400

    Новый PHP_URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-export const PHP_URL = "http://zadovskii.ru/php/";
-// export const PHP_URL = "https://yogasequencebuilder.online/php/";
+export const PHP_URL = "https://yogasequencebuilder.online/php/";
 export const TOUCH_DND_START_TIMEOUT = 1000;
