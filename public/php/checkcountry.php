62620d58eeee5ca0e729db7833f072f6d11613b3
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-28 22:27:23 +0400

    Рефакторинг: перенес sxgeo в подкаталог

--- public/php/checkcountry.php
+++ public/php/checkcountry.php
@@ -1,9 +1,9 @@
 <?php
 require_once('config.php');
-require_once('SxGeo.php');
+require_once('sxgeo/SxGeo.php');
 
 $ip = $_SERVER['REMOTE_ADDR'];
-$SxGeo = new SxGeo('SxGeo.dat');
+$SxGeo = new SxGeo('sxgeo/SxGeo.dat');
 $country = $SxGeo->getCountry($ip);
 
 echo (json_encode($country == 'RU' ? 'ru' : 'en'));
