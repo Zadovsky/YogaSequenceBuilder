65e5ed6b26366aeea64b3aba6f7bdfe3fbb8da57
Zadovsky <49401915+Zadovsky@users.noreply.github.com>
2020-10-28 13:17:03 +0400

    Убрал лишний код и баг

--- public/php/mailer.php
+++ public/php/mailer.php
@@ -5,8 +5,6 @@ require_once('phpmailer/smtp.php');
 require_once('phpmailer/exception.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 date_default_timezone_set('Etc/UTC');
 
@@ -19,28 +17,23 @@ $text = $data['text'];
 
 $mail = new PHPMailer(true);
 
-try {
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host = "smtp.mail.ru";
-    $mail->SMTPAuth = true;
-    $mail->Username = "vlad@yogasequencebuilder.online";
-    $mail->Password = "pXtI3R?jQ8ls";
-    $mail->SMTPSecure = "ssl";
-    $mail->Port = 465;
-    $mail->setFrom("vlad@yogasequencebuilder.online"); // Ваш Email
-    $mail->addAddress($email); // Email получателя
+$mail->isSMTP();
+$mail->Host = "smtp.mail.ru";
+$mail->SMTPAuth = true;
+$mail->Username = "vlad@yogasequencebuilder.online";
+$mail->Password = "pXtI3R?jQ8ls";
+$mail->SMTPSecure = "ssl";
+$mail->Port = 465;
+$mail->setFrom("vlad@yogasequencebuilder.online"); // Ваш Email
+$mail->addAddress($email); // Email получателя
 
-    // Письмо
-    $mail->isHTML(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->Subject = '=?UTF-8?B?' . base64_encode($subj) . '?='; // Заголовок письма
-    $mail->Body = $text; // Текст письма
+// Письмо
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+$mail->Subject = '=?UTF-8?B?' . base64_encode($subj) . '?='; // Заголовок письма
+$mail->Body = $text; // Текст письма
 
-    // Результат
-    $mail->send();
-} catch (Exception $e) {
-    file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/debug.txt', $mail->ErrorInfo);
-}
+// Результат
+$mail->send();
 
 echo (json_encode(true));
